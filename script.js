9f467621d5d32eddeb3a57bb513bdedc542f4d9e
Anand Singh <43192082+the-anand-singh@users.noreply.github.com>
2023-12-13 13:34:34 +0530

    display time

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-function time(){
-
-    var currentdate = new Date(); 
-    var timenow = + currentdate.getHours() + ":"  
-            + currentdate.getMinutes() + ":" 
-            + currentdate.getSeconds();
-        
-    document.getElementById("you").innerHTML="time is ="+timenow;
-
+setInterval(showTime, 1000);
+ 
+// Defining showTime funcion
+function time() {
+    // Getting current time and date
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+ 
+    // Setting time for 12 Hrs format
+    if (hour >= 12) {
+        if (hour > 12) hour -= 12;
+        am_pm = "PM";
+    } else if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+ 
+    hour =
+        hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+ 
+    let currentTime =
+        hour +
+        ":" +
+        min +
+        ":" +
+        sec +
+        am_pm;
+ 
+    // Displaying the time
+    document.getElementById("clock").innerHTML = currentTime;
 }
